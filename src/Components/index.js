e3653a44f95f51d202d983bd6c69b875e5f4111e
Sir Kam'ron Wilson <sirkamronwilson00@gmail.com>
2021-09-10 14:27:37 -0500

    feat: first-commit

---
+++ src/Components/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import NavBar from "./NavComponents/nav-bar";
+
+const Index = () => {
+  return <NavBar />;
+};
+
+export default Index;
