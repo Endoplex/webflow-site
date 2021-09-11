da418ff412f1f2ae21559c4ac2aff1cc1f5752d0
Sir Kam'ron Wilson <sirkamronwilson00@gmail.com>
2021-09-11 00:39:46 -0500

    removing react

--- src/Components/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import { Flex } from "@chakra-ui/react";
-import NavBar from "./NavComponents/nav-bar";
-
-const Index = () => {
-  return <NavBar />;
-};
-
-export default Index;
